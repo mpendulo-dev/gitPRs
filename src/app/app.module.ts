aa697d475fcc203fab25fe460c56730460dfba7f
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-05-31 15:40:30 +0200

    created a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MpenduloComponent } from './components/mpendulo/mpendulo.component';
 import { TitleComponent } from './components/title/title.component';
+import { ThilivhaliComponent } from './components/thilivhali/thilivhali.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MpenduloComponent,
-    TitleComponent
+    TitleComponent,
+    ThilivhaliComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/thilivhali/thilivhali.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thilivhali',
+  templateUrl: './thilivhali.component.html',
+  styleUrls: ['./thilivhali.component.css']
+})
+export class ThilivhaliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
